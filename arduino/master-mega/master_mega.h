bd028db0de77cf7ffc38417997ffc4a97f4e5209
Vicente Medeiros <vicente.mcarvalho@gmail.com>
2016-09-06 13:36:34 -0300

    new file:   arduino/master-mega/master_mega.cpp

---
+++ arduino/master-mega/master_mega.h
@@ -0,0 +1,22 @@
+// Only modify this file to include
+// - function definitions (prototypes)
+// - include files
+// - extern variable definitions
+// In the appropriate section
+
+#ifndef _master_mega_H_
+#define _master_mega_H_
+#include "Arduino.h"
+//add your includes for the project master_mega here
+
+
+//end of add your includes here
+
+
+//add your function definitions for the project master_mega here
+
+
+
+
+//Do not add code below this line
+#endif /* _master_mega_H_ */
