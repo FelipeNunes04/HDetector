61aedd6776ec6af3ddddb47176dcc8ff8ebd3bcc
Ivanildo Bandeira <ivanildolb25@gmail.com>
2016-09-07 18:58:47 -0300

    Update firmware_mega.ino
    Foi estabelecida a comunicação entre o Arduino Mega e a Shield GSM, assim como, organização do código..

--- firmware_mega.ino
+++ firmware_mega.ino
@@ -1,37 +1,53 @@
-/*
- * Programa executado no Arduino Mega 
- */
-
+#include "SIM900.h"
+#include <SoftwareSerial.h>
+#include "call.h"
 #include <SPI.h>
 #include <MFRC522.h>
 #include <LiquidCrystal.h>
 
-const int rele1 = 40; //Led no pino 13 (Relé 1)
-const int rele2 = 32;  //Pino ligado ao Buzzer (Relé 2)
-const int buzzer = 8; // Buzzer do RFID
+CallGSM call;
+
+const int rele1 = 24; //Led simbolizando a lampada (Relé 1)
+const int rele2 = 25;  //Pino ligado ao Buzzer (Relé 2)
 const int pinopir = 22; //Pino ligado ao sensor PIR
+const int buzzer = 23; // Buzzer do RFID
 const int ldrPin = 0; //LDR no pino analógico 0
 int ldrValor = 0; //Valor lido do LDR
 int acionamento = 0; //Valor do sensor de movimento
-boolean movimento = false;
-int recByte;
+boolean movimento = false; //Flag que controla o loop do sensor de movimento
+boolean chamada = false; //Flag que controla o loop da chamada telefonica conforme a presença de movimento     
 
+// Configuração de pinos do RFID
 #define SS_PIN 53
 #define RST_PIN 9
 MFRC522 mfrc522(SS_PIN, RST_PIN);   // Create MFRC522 instance.
- 
-LiquidCrystal lcd(6, 7, 5, 4, 3, 2); 
+
+// Configuração de pinos do LCD
+LiquidCrystal lcd(12, 11, 5, 4, 3, 2);
 char st[20];
+
+void liga_desliga_GSMShield()
+{
+  Serial.print(F("Aguarde..."));
+  pinMode(6, OUTPUT);
+  digitalWrite(6, LOW);
+  delay(1000);
+  digitalWrite(6, HIGH);
+  delay(1000);
+  Serial.println(F("OK!"));
+  digitalWrite(6, LOW);
+  delay(500);
+}
 
 void mensageminicial()
 {
   lcd.clear();
-  lcd.print(" Aproxime o seu");  
+  lcd.print("Monitorando.....");    
   lcd.setCursor(0,1);
-  lcd.print("cartao do leitor");  
+  lcd.print("Nada Detectado");
 }
 
-void desligarSensores() 
+void lerRFID() 
 {
   // Look for new cards
   if ( ! mfrc522.PICC_IsNewCardPresent()) 
@@ -62,26 +78,10 @@ void desligarSensores()
     //Ligando o buzzer com uma frequencia de 1500 hz.
     tone(buzzer,1500);   
     delay(500);
-        
-    Serial.println("Ola Chaveiro !");
-    Serial.println();
-    lcd.clear();
-    lcd.setCursor(0,0);
-    lcd.print("Ola Chaveiro !");
-    lcd.setCursor(0,1);
-    lcd.print("Sensores OFF!");
-    
-    //DESLIGANDO SENSORES PIR
-    digitalWrite(pinopir, LOW);    
-    movimento = false;
-    digitalWrite(rele1,HIGH);    
-    digitalWrite(rele2,HIGH);  
-    
-    //Desligando o buzzer.
     noTone(buzzer);
-    
-    delay(2000);
-    mensageminicial();
+        
+    Serial.println("Ola Chaveiro !");     
+    desligarAlarme();    
   }
  
   if (conteudo.substring(1) == "6B 05 EE 75") //UID 2 - Cartao
@@ -89,105 +89,111 @@ void desligarSensores()
     //Ligando o buzzer com uma frequencia de 1500 hz.
     tone(buzzer,1500);   
     delay(500);
-    
-    Serial.println("Ola Cartao !");
-    Serial.println();
-    lcd.clear();
-    lcd.setCursor(0,0);
-    lcd.print("Ola Cartao !");
-    lcd.setCursor(0,1);
-    lcd.print("Sensores OFF !");
-
-    //DESLIGANDO SENSORES PIR
-    digitalWrite(pinopir, LOW);    
-    movimento = false;
-    digitalWrite(rele1,HIGH);    
-    digitalWrite(rele2,HIGH);  
-    
-    //Desligando o buzzer.
     noTone(buzzer);
     
-    delay(2000);
-    mensageminicial();
+    Serial.println("Ola Cartao !");  
+    desligarAlarme();  
   }
 } 
- 
+
+void desligarAlarme(){
+  //Altera o estado do sensor de movimento
+  digitalWrite(pinopir, LOW); 
+  movimento = false;
+
+  //Desliga os Relés
+  digitalWrite(rele1,HIGH);    
+  digitalWrite(rele2,HIGH);  
+
+  //Altera a mensagem do LCD
+  lcd.clear();
+  lcd.setCursor(0,0);
+  lcd.print("Alarme Desligado");  
+
+  delay(2000);
+  mensageminicial();
+}
+
+void realizarChamada(){
+  Serial.println("Discando...");
+  lcd.clear();  
+  lcd.print("Movimento Detec.");    
+  lcd.setCursor(0,1);
+  lcd.print("Discando...");
+            
+  //Efetua a chamada formato call.Call(<numero a ser chamado>)
+  call.Call("03189999722963");
+  delay(2000);
+          
+  Serial.println("Chamada Efetuada!");
+            
+  call.HangUp();                        
+  chamada = false;                          
+}
+
 void setup()
 {
-     pinMode(pinopir, INPUT); //Define pino sensor como entrada     
-     pinMode(rele1, OUTPUT); //Led indicador de movimento
-     pinMode(rele2,OUTPUT);  // buzzer do PIR   
-     Serial.begin(9600);
-     //Estado inicial dos reles - desligados
+    Serial.begin(9600);
+    Serial.print("Ligando shield GSM SIM900. ");
+    liga_desliga_GSMShield();
+    Serial.println("Testando GSM Shield...");
+    //Comunicacao com o Shield GSM a 2400 bauds
+    if (gsm.begin(2400))
+      Serial.println("nstatus=READY");
+    else Serial.println("nstatus=IDLE");
+  
+    pinMode(pinopir, INPUT); //Define pino sensor como entrada     
+    pinMode(rele1, OUTPUT); //Led indicador de movimento
+    pinMode(rele2,OUTPUT);  // buzzer do PIR   
+    pinMode(buzzer,OUTPUT);
+        
+    //Estado inicial dos reles - desligados
     digitalWrite(rele1, HIGH);
     digitalWrite(rele2, HIGH);    
-    pinMode(buzzer,OUTPUT);
-
+    
     SPI.begin();      // Inicia  SPI bus
     mfrc522.PCD_Init();   // Inicia MFRC522
     lcd.begin(16, 2); 
     mensageminicial(); 
 }
  
-void loop()
-{
+void loop(){
   ///ler o valor do LDR
   ldrValor = analogRead(ldrPin); //O valor lido será entre 0 e 1023
   Serial.print("Valor LDR: ");
   Serial.println(ldrValor);
 
   acionamento = digitalRead(pinopir); //Le o valor do sensor PIR  
-    
-  if (acionamento == HIGH) // SE Movimento
-  {    
+
+  // Se ocorrer Movimento 
+  if (acionamento == HIGH) {    
     movimento = true;
-        
+    chamada = true;      
     while(movimento){
-      Serial.println("Movimento!!");    
-      boolean sms = true;
+      Serial.println("Movimento!!");          
+      lcd.clear();
+      lcd.print("Movimento Detec.");    
+      lcd.setCursor(0,1);  
+      lcd.print("Alarme Ativado!");
       //se o valor lido for maior que 500, liga o led e aciona o buzzer
       if (ldrValor>= 500) {
         //Ligando o buzzer e o led
-        digitalWrite(rele2,LOW);   
         digitalWrite(rele1,LOW);   
-        if(sms){
-          Serial.write('1');           
-          if(Serial.available() > 0){
-            recByte = Serial.read();        
-            if(recByte == '1'){
-              lcd.clear();
-              lcd.setCursor(0,0);
-              lcd.print("SMS enviado !");
-              Serial.println("SMS enviado !");
-              sms = false;              
-              delay(2000);
-              mensageminicial();
-            }                                        
-          }
-        }        
-        delay(2000);
+        digitalWrite(rele2,LOW);
+               
+        if(chamada){
+          realizarChamada();
+        }
       } else{      
         //Ligando o buzzer
         digitalWrite(rele2,LOW);       
-        if(sms){
-          Serial.write('1');
-            if(Serial.available() > 0){
-            recByte = Serial.read();        
-            if(recByte == '1'){
-              lcd.clear();
-              lcd.setCursor(0,0);
-              lcd.print("SMS enviado !");
-              Serial.println("SMS enviado !");
-              sms = false;              
-              delay(2000);
-              mensageminicial();
-            }                                        
-          }        
-        }        
-        delay(2000);
-      }  
-      desligarSensores();
-    }     
-  }
-  delay(2000);    
+        if(chamada){
+          realizarChamada();                    
+        }                                        
+      } 
+      delay(1000);
+      lerRFID();          
+    }       
+  }         
+  delay(3000);    
 }
