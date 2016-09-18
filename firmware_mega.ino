f12c013154bc52347695240640ca5058d69e5a2d
Ivanildo Bandeira <ivanildolb25@gmail.com>
2016-09-18 17:41:15 -0300

    Update firmware_mega.ino

--- firmware_mega.ino
+++ firmware_mega.ino
@@ -1,21 +1,31 @@
-#include "SIM900.h"
 #include <SoftwareSerial.h>
-#include "call.h"
-#include <SPI.h>
-#include <MFRC522.h>
 #include <LiquidCrystal.h>
+#include <Ultrasonic.h>
+#include <MFRC522.h>
+#include <SPI.h>
+#include "SIM900.h"
+#include "sms.h"
 
-CallGSM call;
+SMSGSM sms;
 
-const int rele1 = 24; //Led simbolizando a lampada (Relé 1)
-const int rele2 = 25;  //Pino ligado ao Buzzer (Relé 2)
+const int rele_led = 24; //Led simbolizando a lampada (Relé 1)
+const int rele_buzzer = 25;  //Pino ligado ao Buzzer (Relé 2)
+const int rele_lcd = 13;  //Pino ligado ao LCD (Relé 3)
 const int pinopir = 22; //Pino ligado ao sensor PIR
 const int buzzer = 23; // Buzzer do RFID
 const int ldrPin = 0; //LDR no pino analógico 0
 int ldrValor = 0; //Valor lido do LDR
 int acionamento = 0; //Valor do sensor de movimento
 boolean movimento = false; //Flag que controla o loop do sensor de movimento
-boolean chamada = false; //Flag que controla o loop da chamada telefonica conforme a presença de movimento     
+boolean smsFlag = false; //Flag que controla o loop do SMS conforme a presença de movimento     
+
+SoftwareSerial mySerial(18, 19); // RX, TX
+
+//Define os pinos para o trigger e echo (Sensor Ultrassonico)
+#define pino_trigger 6
+#define pino_echo 7
+//Inicializa o sensor nos pinos definidos acima
+Ultrasonic ultrasonic(pino_trigger, pino_echo);
 
 // Configuração de pinos do RFID
 #define SS_PIN 53
@@ -102,62 +112,83 @@ void desligarAlarme(){
   movimento = false;
 
   //Desliga os Relés
-  digitalWrite(rele1,HIGH);    
-  digitalWrite(rele2,HIGH);  
+  digitalWrite(rele_led,HIGH);    
+  digitalWrite(rele_buzzer,HIGH);  
 
   //Altera a mensagem do LCD
   lcd.clear();
   lcd.setCursor(0,0);
   lcd.print("Alarme Desligado");  
 
-  delay(2000);
+  delay(1000);
   mensageminicial();
 }
 
-void realizarChamada(){
-  Serial.println("Discando...");
+void enviarSMS(){
+  Serial.println("Enviando SMS...");
   lcd.clear();  
   lcd.print("Movimento Detec.");    
   lcd.setCursor(0,1);
-  lcd.print("Discando...");
+  lcd.print("Enviando SMS...");
             
-  //Efetua a chamada formato call.Call(<numero a ser chamado>)
-  call.Call("03189999722963");
-  delay(2000);
-          
-  Serial.println("Chamada Efetuada!");
-            
-  call.HangUp();                        
-  chamada = false;                          
+  //Envia um SMS para o numero selecionado. Formato sms.SendSMS(<numero>,<mensagem>)
+  if (sms.SendSMS("02189999722963", "ALERTA!!! Movimento Detectado..."))
+    Serial.println("SMS enviado!");          
+  
+  smsFlag = false;                          
+}
+
+// Função que desliga o LCD quando fechar a tampa da caixa
+void monitoraLCD(){
+  //Le as informacoes do sensor em cm
+  float cmMsec;
+  long microsec = ultrasonic.timing();
+  cmMsec = ultrasonic.convert(microsec, Ultrasonic::CM);
+  
+  //Exibe informacoes no serial monitor
+  Serial.print("Distancia em cm: ");
+  Serial.print(cmMsec);
+  Serial.print(" ");
+  
+  if(cmMsec <= 5.0){
+    // Turn off the display:
+    digitalWrite(rele_lcd, HIGH);    
+  }else{
+    digitalWrite(rele_lcd, LOW);        
+  }  
 }
 
 void setup()
 {
     Serial.begin(9600);
+    mySerial.begin(9600);
     Serial.print("Ligando shield GSM SIM900. ");
     liga_desliga_GSMShield();
     Serial.println("Testando GSM Shield...");
     //Comunicacao com o Shield GSM a 2400 bauds
-    if (gsm.begin(2400))
-      Serial.println("nstatus=READY");
-    else Serial.println("nstatus=IDLE");
+    if (gsm.begin(9600)){            
+      Serial.println("\nstatus=READY");            
+    }else Serial.println("\nstatus=IDLE");
   
     pinMode(pinopir, INPUT); //Define pino sensor como entrada     
-    pinMode(rele1, OUTPUT); //Led indicador de movimento
-    pinMode(rele2,OUTPUT);  // buzzer do PIR   
+    pinMode(rele_led, OUTPUT); //Led indicador de movimento
+    pinMode(rele_buzzer,OUTPUT);  // buzzer do PIR   
+    pinMode(rele_lcd,OUTPUT);  // Vcc do LCD   
     pinMode(buzzer,OUTPUT);
         
     //Estado inicial dos reles - desligados
-    digitalWrite(rele1, HIGH);
-    digitalWrite(rele2, HIGH);    
+    digitalWrite(rele_led, HIGH);
+    digitalWrite(rele_buzzer, HIGH);    
+    digitalWrite(rele_lcd, HIGH);    
     
     SPI.begin();      // Inicia  SPI bus
     mfrc522.PCD_Init();   // Inicia MFRC522
-    lcd.begin(16, 2); 
+    lcd.begin(16, 2); // Inicia display LCD
     mensageminicial(); 
 }
  
 void loop(){
+  monitoraLCD();
   ///ler o valor do LDR
   ldrValor = analogRead(ldrPin); //O valor lido será entre 0 e 1023
   Serial.print("Valor LDR: ");
@@ -168,8 +199,9 @@ void loop(){
   // Se ocorrer Movimento 
   if (acionamento == HIGH) {    
     movimento = true;
-    chamada = true;      
+    smsFlag = true;      
     while(movimento){
+      monitoraLCD();
       Serial.println("Movimento!!");          
       lcd.clear();
       lcd.print("Movimento Detec.");    
@@ -178,17 +210,17 @@ void loop(){
       //se o valor lido for maior que 500, liga o led e aciona o buzzer
       if (ldrValor>= 500) {
         //Ligando o buzzer e o led
-        digitalWrite(rele1,LOW);   
-        digitalWrite(rele2,LOW);
+        digitalWrite(rele_led,LOW);   
+        digitalWrite(rele_buzzer,LOW);
                
-        if(chamada){
-          realizarChamada();
+        if(smsFlag){
+          enviarSMS();
         }
       } else{      
         //Ligando o buzzer
-        digitalWrite(rele2,LOW);       
-        if(chamada){
-          realizarChamada();                    
+        digitalWrite(rele_buzzer,LOW);       
+        if(smsFlag){
+          enviarSMS();                    
         }                                        
       } 
       delay(1000);
